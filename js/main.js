d85bc73268fa9162525db823dc8fc6aca276e2b4
Gisele D. F. Santos <zelegz@hotmail.com>
2021-04-22 00:17:01 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function btnAssistirAgora(){
+    window.location.href = "https://www.primevideo.com/detail/0SXJ8ZO8LXB9OUQIZLU6UO8M9Y/ref=dvm_ass_acm_br_ra_s_justwatch_2912969b9b5d48109d6a143ad0d291ce"
+}
+
+function showModalInfo(){
+  document.getElementById("modalInfo").style.display = "block";
+}
+
+function closeModalInfo(){
+  document.getElementById("modalInfo").style.display = "none";
+}
